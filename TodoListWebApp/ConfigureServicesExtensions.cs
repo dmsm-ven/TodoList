75ca3466068609674106300288eee406fb321529
Painven <painven@gmail.com>
2025-04-14 01:40:25 +0300

    Добавлен новый проект web app razor pages

---
+++ TodoListWebApp/ConfigureServicesExtensions.cs
@@ -0,0 +1,23 @@
+using TodoListApp.Core;
+using TodoListApp.DataAccess.Repositories.Interfaces;
+using TodoListApp.DataAccess.Repositories.Postgres.Base;
+using TodoListApp.DataAccess.Repositories.Postgres.Repositories;
+
+public static class ConfigureServicesExtensions
+{
+    public static IServiceCollection ConfigureMyDatabaseRepositories(this IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("default");
+        services.AddTransient<IDapperDatabaseAccess>(x => new PostgresDapperDatabaseAccess(connectionString));
+        services.AddSingleton<IUserDataEncryptValidator, BCryptUserDataValidator>();
+
+        services.AddTransient<IAppLogger, PostgresAppLogger>();
+        services.AddTransient<IUserRepository, PostgresBCryptUserValidator>();
+        services.AddTransient<IEmployeerRepository, PostgresEmployeerRepository>();
+        services.AddTransient<IEmployeerPaymentRepository, PostgresEmployeerPaymentRepository>();
+        services.AddTransient<IJobItemRepository, PostgresJobItemRepository>();
+        services.AddTransient<ISettingsRepository, PostgresSettingsRepository>();
+
+        return services;
+    }
+}
---
+++ TodoListWebApp/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureMyDatabaseRepositories(builder.Configuration);
+builder.Services.AddRazorPages();
+
+var app = builder
+    .Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
