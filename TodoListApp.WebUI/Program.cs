f5de77c9173885cf506ee64b99146a3b9f3651c1
Painven <painven@gmail.com>
2025-12-15 11:49:51 +0300

    WEB UI простая версия без авторизации

--- TodoListApp.WebUI/Program.cs
+++ TodoListApp.WebUI/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using TodoListApp.ApiClient;
+using TodoListApp.Core.Repositories.Interfaces;
 using TodoListApp.WebUI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +8,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient(nameof(TodoListAppApiClient), client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["API_HOST"] ?? throw new ArgumentException("API HOST must be provided"));
+    client.DefaultRequestHeaders.Add("X-API-KEY", builder.Configuration["API_KEY"] ?? throw new ArgumentException("API KEY must be provided"));
+});
+builder.Services.AddSingleton<TodoListAppApiClient>(sp =>
+{
+    var factory = sp.GetRequiredService<IHttpClientFactory>();
+    var httpClient = factory.CreateClient(nameof(TodoListAppApiClient));
+    return new TodoListAppApiClient(httpClient);
+});
+builder.Services.AddSingleton<IAppLogger>(x => x.GetRequiredService<TodoListAppApiClient>());
+builder.Services.AddSingleton<IJobItemRepository>(x => x.GetRequiredService<TodoListAppApiClient>());
+builder.Services.AddSingleton<IEmployeerRepository>(x => x.GetRequiredService<TodoListAppApiClient>());
 
 var app = builder.Build();
 
