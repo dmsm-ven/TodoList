25d695b38489d148d655ed674b3af084bc0b1b1b
Painven <painven@gmail.com>
2026-01-04 11:53:43 +0300

    Добавлен stopwatch длительности загрузки приложения в title главного окна

--- TodoListApp.Desktop/ViewModels/Windows/MainWindowViewModel.cs
+++ TodoListApp.Desktop/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,14 +1,32 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace TodoListApp.Desktop.ViewModels.Windows;
 
 public partial class MainWindowViewModel : ObservableObject
 {
     [ObservableProperty]
+    private string title;
+
+    [ObservableProperty]
     private TodoListViewModel todoListViewModel;
+
+    Stopwatch loadingTime;
 
     public MainWindowViewModel(TodoListViewModel todoListViewModel)
     {
+        loadingTime = Stopwatch.StartNew();
         TodoListViewModel = todoListViewModel;
+        TodoListViewModel.PropertyChanged += TodoListViewModel_PropertyChanged;
+    }
+
+    private void TodoListViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(TodoListViewModel.IsLoading) && !TodoListViewModel.IsLoading)
+        {
+            loadingTime.Stop();
+            Title = $"Список дел | Загрузка выполнена за {(int)loadingTime.Elapsed.TotalMilliseconds} мс.";
+        }
     }
 }
