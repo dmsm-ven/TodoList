5461aad2b2153f9dfbd51b75ec38ee26db4624b9
Painven <painven@gmail.com>
2026-03-12 14:22:26 +0300

    Revert "удален blazor проект"
    This reverts commit 312e8afe06f3ef74e8ce08027dec8113d8384aad.

---
+++ TodoListApp.WebUI/DiExtensions.cs
@@ -0,0 +1,42 @@
+﻿using TodoListApp.ApiClient;
+using TodoListApp.WebUI.Models;
+
+public static class AuthDefaults
+{
+    public const string AuthScheme = "login_page_cookie_auth";
+}
+
+public static class DiExtensions
+{
+
+    public static WebApplicationBuilder ResolveAppDependencies(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<AppUserLoginConfiguration>(builder.Configuration.GetSection(nameof(AppUserLoginConfiguration)));
+        builder.Services.AddHttpClient(nameof(TodoListAppApiClient), client =>
+        {
+            client.BaseAddress = new Uri(builder.Configuration["ApiConfiguration:Host"] ?? throw new ArgumentException("API HOST must be provided"));
+            client.DefaultRequestHeaders.Add("X-API-KEY", builder.Configuration["ApiConfiguration:Token"] ?? throw new ArgumentException("API KEY must be provided"));
+        });
+        builder.Services.AddSingleton<TodoListAppApiClient>(sp =>
+        {
+            var factory = sp.GetRequiredService<IHttpClientFactory>();
+            var httpClient = factory.CreateClient(nameof(TodoListAppApiClient));
+            return new TodoListAppApiClient(httpClient);
+        });
+
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddNotificatorSender(this WebApplicationBuilder builder)
+    {
+        //builder.Services.AddHttpClient();
+        //builder.Services.Configure<GeoDataExtractorConfiguration>(builder.Configuration.GetSection(nameof(GeoDataExtractorConfiguration)));
+        //builder.Services.Configure<GeoDataExtractorConfiguration>(builder.Configuration.GetSection(nameof(TelegramConfiguration)));
+        //builder.Services.AddSingleton<IGeoDataExtractor, BasicGeoDataExtractor>();
+        //builder.Services.AddSingleton<IApiEventNotificator, TelegramApiEventNotificator>();
+
+        return builder;
+    }
+
+}
---
+++ TodoListApp.WebUI/Models/AppUserLoginConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListApp.WebUI.Models;
+
+public class AppUserLoginConfiguration
+{
+    public string Token { get; set; }
+}
+
+public class LoginModel
+{
+    [Required(ErrorMessage = "UserToken is required")]
+    public string UserToken { get; set; }
+}
